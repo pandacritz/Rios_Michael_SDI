a5f629381a9697ba430a2e9501ed8c245d83ec0f
Michael Rios <mdrocks54@gmail.com>
2014-09-06 15:50:37 -0400

    firstcommit

---
+++ sdi-lab2/rios_michael_lab2.js
@@ -0,0 +1,21 @@
+alert("JavaScript works!");
+
+// My variables 
+var account = "Michael Rios";
+var game   = "Ysera US";
+var Faction = "Alliance";
+var Race  =  "Panderan";
+
+// My confirm
+person = confirm ("Are you Pandacritz? \nClick Ok for Yes, Cancel for no");
+game = prompt ("Please type your realm:", "Ysera US");
+faction = parseFloat(prompt("What faction are you?" , "The Alliance"));
+race = prompt ("Selected race Panderan:" , "monk");
+
+// My Outputs
+console.log ("My legal name is " + account + ".");
+console.log ("The realm I play on is " + game + ".");
+console.log ("The Faction I play as is " + Faction + ".");
+console.log ("Since their are only two race options for monks I play " + Race + ".");
+
+
